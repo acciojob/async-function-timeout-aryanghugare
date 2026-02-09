0d2390844f77eba8bd136c7fb0893336d491a8c1
Aryan Ghugare <aryanghugare75@gmail.com>
2026-02-09 10:34:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+const text = document.querySelector("#text");
+const delay = document.querySelector("#delay");
+const submitBtn = document.querySelector("#btn");
+const output = document.querySelector("#output");
+
+submitBtn.addEventListener("click",async (e)=>{
+	e.preventDefault();
+	let textDisplay = text.value ;
+	let delayDisplay = parseInt(delay.value);
+	await display(textDisplay,delayDisplay);
+	text.value = ""
+	delay.value = ""
+	
+})
+
+async function display(text,delay){
+	setTimeout(()=>{
+		output.innerHTML = text ;
+	},delay)
+	
+}
+
